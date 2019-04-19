8ae148a6124e19fd785dbb4a8710eb56c0d02acf
drd4n <the_dann@hotmail.co.th>
2019-04-20 01:05:05 +0700

    :yellow_heart:added footer

---
+++ src/components/index/footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Footer = styled.footer `
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1em;
+    text-align: left;
+`
+
+const StyledFooter = (props)=>(
+    <Footer>
+    
+    Powered by FireBase<br></br>
+    Created by KhaKai United(Dr.D4N, PoonnyNH, KONMEKLOYDAI, Chickenrap_)<br></br>
+    Contact us: <br></br>
+    &nbsp;e-mail: poon.002@hotmail.com<br></br>
+    &nbsp;SoundCloud: www.SoundCloud.com/drd4nx<br></br>
+    &nbsp;IG: drd4nx<br></br>
+        
+    </Footer>
+)
+
+export default StyledFooter
