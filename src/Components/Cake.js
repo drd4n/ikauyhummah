29de5729b0a19e105d2e457a09b08ee6f2075edd
drd4n <the_dann@hotmail.co.th>
2019-04-21 04:03:33 +0700

    :yellow_heart:fix pictures

--- src/Components/Cake.js
+++ src/Components/Cake.js
@@ -24,7 +24,7 @@ class Cake extends React.Component {
   render() {
     return (
       
-        <CakeImg src="https://scontent.fbkk8-2.fna.fbcdn.net/v/t1.0-9/52908248_304817086889615_5342276976432381952_n.jpg?_nc_cat=107&_nc_eui2=AeHky1joq_IZhUE6-dsrg5yB5_nvxcT5k0dQ-dDnx654C3VrDHBsZquJWLzl_5pxaWXWef4tLyZ181UjA0rbTztjeCFYhTS0u3-EBJsUlpMw2A&_nc_ht=scontent.fbkk8-2.fna&oh=c07ad367ffb00246597bd31b8e31eba6&oe=5D475AC1" alt="The Cake" />
+        <CakeImg src={require('./pictures/cake.png')} alt="The Cake" />
 
       
     );
