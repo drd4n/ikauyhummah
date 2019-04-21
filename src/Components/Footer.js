229cc57227c071571e891544f671dbb85aeb2b45
ZolyKana <poon.roon3@mail.kmutt.ac.th>
2019-04-21 10:43:15 +0700

    footer sponsors :moneybag:

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,18 +7,38 @@ const Footer = styled.footer `
     text-align: left;
     font-family: 'Montserrat', sans-serif;
 `
+const Sponsor = styled.img`
+  width: auto;
+  height: 50px;
+`
+const Hack = styled.img`
+  width: auto;
+  height: 80px;
+`
 
 const StyledFooter = (props)=>(
     <Footer>
     <hr></hr>
-    Powered by FireBase<br></br>
-    Created by KhaKai United(Dr.D4N, PoonnyNH, KONMEKLOYDAI, Chickenrap_)<br></br>
+    Created by KhaKai United<br></br>
     Contact us:
     <ul>
-    <li>e-mail: poon.002@hotmail.com</li>
+    <li>e-mail: freedompoon@gmail.com</li>
     <li>SoundCloud: www.SoundCloud.com/drd4nx</li>
-    <li>IG: drd4nx</li>
+    <li>IG: drd4nx , poonynh_ , konmeklotdai , Chickenrap_ </li>
     </ul> 
+    <hr></hr>
+    powered by : &nbsp;&nbsp;<Sponsor src={require('./pictures/sponsors/logo_Asset 53.png')} /> &nbsp;&nbsp;
+    <Sponsor src={require('./pictures/sponsors/logo_Asset 54.png')} /> &nbsp;&nbsp;
+    <Sponsor src={require('./pictures/sponsors/logo_Asset 55.png')} /> &nbsp;&nbsp;
+    <Sponsor src={require('./pictures/sponsors/logo_Asset 56.png')} /> &nbsp;&nbsp;
+    <Sponsor src={require('./pictures/sponsors/logo_Asset 57.png')} /><br>
+    </br> &nbsp;&nbsp;
+    <Sponsor src={require('./pictures/sponsors/logo_Asset 58.png')} /> &nbsp;&nbsp;
+    <Sponsor src={require('./pictures/sponsors/logo_Asset 59.png')} /> &nbsp;&nbsp;
+    <Sponsor src={require('./pictures/sponsors/logo_Asset 60.png')} /> &nbsp;&nbsp;
+    <Sponsor src={require('./pictures/sponsors/logo_Asset 61.png')} /> &nbsp;&nbsp;
+    <Sponsor src={require('./pictures/sponsors/logo_Asset 62.png')} /> &nbsp;&nbsp;
+    <Hack src={require('./pictures/sponsors/logo_Asset 63.png')} /> 
     </Footer>
 )
 
