486b8895662b8accedf84c9eb94189c5b0665664
drd4n <the_dann@hotmail.co.th>
2019-04-21 09:07:35 +0700

    add cake

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Summit from './Components/Summit'
 import HeaderText from './Components/HeaderText';
 import Footer from './Components/Footer'
 import Cake from './Components/Cake'
-import Gifts from './Components/Gifts'
+// import Gifts from './Components/Gifts'
 
 class App extends Component {
   render() {
